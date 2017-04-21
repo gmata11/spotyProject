7959848e17b6cfdeb2f9cc2741b0a5bbf0710f75
gmata11 <gerardmc12@gmail.com>
2017-04-21 18:29:35 +0200

    Modificat l'arxiu models.py

--- appmusic/admin.py
+++ appmusic/admin.py
@@ -1,8 +1,6 @@
 from django.contrib import admin
-from .models import Library, Artist, Album, Track, LibraryReview
+from .models import Artist, Album, Track
 
-admin.site.register(Library)
 admin.site.register(Artist)
 admin.site.register(Album)
 admin.site.register(Track)
-admin.site.register(LibraryReview)
